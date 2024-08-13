02157eff320f13e8bfb18bd363f08e2164df5468
FhRh <yiyop@naver.com>
2024-08-14 08:04:53 +0900

    feat : provinceId 넘겨주기

--- dream/src/main/java/com/example/dream/province/dto/ProvincePreviewDto.java
+++ dream/src/main/java/com/example/dream/province/dto/ProvincePreviewDto.java
@@ -3,12 +3,14 @@ package com.example.dream.province.dto;
 import com.example.dream.province.domain.Province;
 
 public record ProvincePreviewDto(
+        Long provinceId,
         String name,
         String image,
         int likesCount
 ) {
     public static ProvincePreviewDto fromWeekLikesProvince(Province province){
         return new ProvincePreviewDto(
+                province.getId(),
                 province.getName(),
                 province.getProfileImage(),
                 province.getWeekLikesCounts()
@@ -17,6 +19,7 @@ public record ProvincePreviewDto(
 
     public static ProvincePreviewDto fromTotalLikesProvince(Province province){
         return new ProvincePreviewDto(
+                province.getId(),
                 province.getName(),
                 province.getProfileImage(),
                 province.getTotalLikesCounts()
