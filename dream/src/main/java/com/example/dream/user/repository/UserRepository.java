981b378763636f5bc99a0a3adac130455348533e
FhRh <yiyop@naver.com>
2024-08-13 17:33:01 +0900

    fix : user import 수정

--- dream/src/main/java/com/example/dream/user/repository/UserRepository.java
+++ dream/src/main/java/com/example/dream/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.dream.user.repository;
 
-import com.sundaegukbap.banchango.user.domain.User;
+import com.example.dream.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
